759b2a94cd4a02f4a4b31c57c9ba4d293a3539c8
gulshadlgr <64360716+gulshadlgr@users.noreply.github.com>
2020-05-10 14:58:41 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace temelkullanimlar
+namespace Ucus_Rezervasyon_Sistem
 {
     public partial class Form1 : Form
     {
@@ -19,32 +19,18 @@ namespace temelkullanimlar
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Vatan!Millet!Sakarya!");
+            listBox1.Items.Add("Rota: " + comboBox1.Text + "-" + comboBox2.Text + "Tarih: " + dateTimePicker1.Text + "Saat: " + maskedTextBox1.Text + "Yolcu Bilgileri- Ad " + textBox1.Text + "TC- " + maskedTextBox1.Text + "Tel- " + maskedTextBox2.Text);
+
+
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            label2.Text = "TÜRKİYE";
-        }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            label6.Text = "40********0";
-            label7.Text = "sevimdulger58@gmail.com";
-                label8.Text = "********";
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            //textBox1.Text = "YAZILIM";
-            label9.Text = textBox1.Text;
-            label10.Text = textBox2.Text;
-            label11.Text = textBox3.Text;
+            label9.Text = comboBox2.Text;
+            comboBox2.Text = comboBox1.Text;
+            comboBox1.Text = label9.Text;
         }
     }
 }
